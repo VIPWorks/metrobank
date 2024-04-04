d07c806133ad939ac520d70fe4512c28b44e29ba
Rahulg078 <rahulg6235035@gmail.com>
2024-04-04 18:27:02 +0530

    home/card.js updated

--- components/sections/home3/Card.js
+++ components/sections/home3/Card.js
@@ -12,8 +12,8 @@ export default function Card() {
                 </div>
                 <div className="auto-container">
                     <div className="sec-title mb_70">
-                    <h6>Our Credit Card</h6>
-                    <h2>Credit Cards We Provide</h2>
+                    <h2>Why Choose CW BANK</h2>
+                    <p>Access to Leading Liquidity Providers: Partnering with CW BANK gives you access to some of the most reputable liquidity providers in the forex industry, ensuring that you have access to deep liquidity and competitive pricing on your trades.</p>
                     </div>
                     <div className="row clearfix">
                     <div className="col-lg-4 col-md-6 col-sm-12 card-block">
@@ -21,8 +21,8 @@ export default function Card() {
                         <div className="inner-box">
                             <figure className="image-box"><Link href="/index-2"><img src="assets/images/resource/card-1.jpg" alt="" /></Link></figure>
                             <div className="lower-content">
-                            <h3><Link href="/index-2">Visa Silver Card</Link></h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis tincidunt</p>
+                            <h3><Link href="/index-2">Robust and Reliable Trading Infrastructure</Link></h3>
+                            <p class="justify">Our advanced trading infrastructure is designed to deliver fast, reliable, and secure order execution, even during periods of high market volatility. With CW BANK, you can trade with confidence, knowing that your orders will be executed promptly and efficiently.</p>
                             <div className="btn-box">
                                 <Link href="/index-2" className="theme-btn btn-two">Apply for Card</Link>
                             </div>
@@ -35,8 +35,8 @@ export default function Card() {
                         <div className="inner-box">
                             <figure className="image-box"><Link href="/index-2"><img src="assets/images/resource/card-2.jpg" alt="" /></Link></figure>
                             <div className="lower-content">
-                            <h3><Link href="/index-2">Mastercard Gold Card</Link></h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis tincidunt</p>
+                            <h3><Link href="/index-2">Transparent and Fair Pricing</Link></h3>
+                            <p class="justify">We believe in transparency and fairness when it comes to pricing. Our pricing is competitive and transparent, with no hidden fees or commissions. You can trust CW BANK to offer you fair and competitive pricing on your trades.</p>
                             <div className="btn-box">
                                 <Link href="/index-2" className="theme-btn btn-two">Apply for Card</Link>
                             </div>
@@ -49,8 +49,8 @@ export default function Card() {
                         <div className="inner-box">
                             <figure className="image-box"><Link href="/index-2"><img src="assets/images/resource/card-3.jpg" alt="" /></Link></figure>
                             <div className="lower-content">
-                            <h3><Link href="/index-2">Visa Platinum Card</Link></h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis tincidunt</p>
+                            <h3><Link href="/index-2">Dedicated Customer Support</Link></h3>
+                            <p class="justify">Our team of experienced customer support professionals is available 24/7 to assist you with any questions or concerns you may have. Whether you need help with technical issues, account inquiries, or trading assistance, we're here to help you every step of the way.</p>
                             <div className="btn-box">
                                 <Link href="/index-2" className="theme-btn btn-two">Apply for Card</Link>
                             </div>
